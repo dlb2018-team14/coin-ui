6bb9386b274e1e0fa7f1f6716fa1f7500559a17e
maeharin <maeharin@gmail.com>
2019-03-21 19:05:12 +0900

    左上に金額表示

--- main_camera.py
+++ main_camera.py
@@ -1,10 +1,14 @@
 from darkflow.net.build import TFNet
 import cv2
+import sys
+import os
+from time import time as timer
+import numpy as np
+
+# see: webcam
+# https://github.com/thtrieu/darkflow#cameravideo-file-demo
 
 options = {
-    # webcam
-    # https://github.com/thtrieu/darkflow#cameravideo-file-demo
-    "demo": "camera",
     # 訓練済みモデル
     "load": "./data/weights/yolo-obj_900.weights",
     # 訓練するときに使ったyoloのコンフィグファイル
@@ -12,9 +16,123 @@ options = {
     # ラベル
     "labels": "./data/labels/obj.names",
     # 閾値
-    "threshold": 0.3
+    "threshold": 0.3,
+    # 何elapsedごとに結果を画面に表示するか
+    "queue": 1
 }
 
 tfnet = TFNet(options)
 
-tfnet.camera()
+
+def start_web_camera(tfnet):
+    SaveVideo = tfnet.FLAGS.saveVideo
+
+    # カメラデバイス番号
+    device_number = 0
+
+    camera = cv2.VideoCapture(device_number)
+    # fps設定
+    #camera.set(cv2.CAP_PROP_FPS, 10)
+    print(camera.get(cv2.CAP_PROP_FPS))
+
+    tfnet.say('Press [ESC] to quit demo')
+
+    assert camera.isOpened(), \
+        'Cannot capture source'
+
+    cv2.namedWindow('', 0)
+    _, frame = camera.read()
+    height, width, _ = frame.shape
+    cv2.resizeWindow('', width, height)
+
+    if SaveVideo:
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        fps = 1 / tfnet._get_fps(frame)
+        if fps < 1:
+            fps = 1
+        videoWriter = cv2.VideoWriter(
+            'video.avi', fourcc, fps, (width, height))
+
+    # buffers for demo in batch
+    buffer_inp = list()
+    buffer_pre = list()
+
+    elapsed = int()
+    start = timer()
+    tfnet.say('Press [ESC] to quit demo')
+    # Loop through frames
+    while camera.isOpened():
+        elapsed += 1
+        # カメラからフレームを読み込む
+        _, frame = camera.read()
+        if frame is None:
+            print ('\nEnd of Video')
+            break
+
+        # 予測前の処理
+        preprocessed = tfnet.framework.preprocess(frame)
+        buffer_inp.append(frame)
+        buffer_pre.append(preprocessed)
+
+        # Only process and imshow when queue is full
+        if elapsed % tfnet.FLAGS.queue == 0:
+            feed_dict = {tfnet.inp: buffer_pre}
+            # 予測！
+            net_out = tfnet.sess.run(tfnet.out, feed_dict)
+            for img, single_out in zip(buffer_inp, net_out):
+                # バウンディングボックスを画像にセット
+                postprocessed = tfnet.framework.postprocess(single_out, img, False)
+                if SaveVideo:
+                    videoWriter.write(postprocessed)
+
+                # 総額計算
+                boxesInfo = tfnet.return_predict(img)
+                labels = [boxInfo["label"].replace('yen', '') for boxInfo in boxesInfo]
+                nums = [int(l.replace('yen', '')) for l in labels if l != 'other']
+                amount = sum(nums)
+                # 金額を書き込む
+                # 背景色
+                #cv2.rectangle(
+                #    postprocessed,
+                #    (0, 0),  # (left, top)
+                #    (300, 150),  # (right, bottom)
+                #    (255, 255, 255),  # color white
+                #    cv2.FILLED
+                #)
+                # テキスト
+                cv2.putText(
+                    postprocessed,  # 書き込み対象画像
+                    "%d yen" % amount,  # 書き込みテキスト
+                    (0, 50),  # org: 書く場所の座標(テキストを書き始める位置の左下)
+                    0,  # fontFace: フォント ( OpenCVが提供するフォントの情報については cv2.putText() 関数のドキュメンテーションを参照のこと)-
+                    2,  # fontScale: フォントサイズ (文字のサイズ)
+                    (0, 0, 200),  # color (red)
+                    5,  # thickness: 線の太さ
+                    # False  # bottomLeftOrigin(第9引数): Trueなら左下隅を原点、そうでなければ左上隅
+                )
+
+                # 結果を画面に表示
+                cv2.imshow('', postprocessed)
+            # Clear Buffers
+            buffer_inp = list()
+            buffer_pre = list()
+
+        if elapsed % 5 == 0:
+            sys.stdout.write('\r')
+            sys.stdout.write('{0:3.3f} FPS'.format(
+                elapsed / (timer() - start)))
+            sys.stdout.flush()
+
+        choice = cv2.waitKey(1)
+        if choice == 27: break
+
+    sys.stdout.write('\n')
+    if SaveVideo:
+        videoWriter.release()
+    camera.release()
+    cv2.destroyAllWindows()
+
+
+start_web_camera(tfnet)
+
+
